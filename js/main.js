b20786d8d5789b89b57f41c743f00d24650aa322
Дмитрий Никитин <franky5326@yandex.ru>
2023-02-17 12:33:14 +0700

    делал 3 столбца сбер ушли

--- js/main.js
+++ js/main.js
@@ -3,29 +3,53 @@ let eventBus = new Vue()
 Vue.component('cols', { 
     template:`
     <div id="cols">
-        <div class="col-wrapper">
-
+    <div class="col-wrapper">
+        <p class="error" v-for="error in errors">{{error}}</p>
         <newcard></newcard>
         <div class="col">
             <ul>
-            <p class="error" v-for="error in errors">{{error}}</p>
-                <li class="cards" v-for="card in column1" ><p>{{ card.title }}</p>
-                    <div>
-                        <ul>
-                            <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
-                                <input @click="t.completed = true" 
-                                class="checkbox" type="checkbox"
-                                :disabled="t.completed">
-                                <p :class="{completed: t.completed}">{{t.title}}</p>
-                            </li>
-                        </ul>
-                    </div>
-                    </li>
+                <li class="cards" v-for="card in column1"><p>{{ card.title }}</p><p>{{ card.status }}</p>
+                    <ul>
+                        <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
+                            <input @click="t.completed = true"
+                            class="checkbox" type="checkbox"
+                            :disabled="t.completed">
+                            <p :class="{completed: t.completed}">{{t.title}}</p>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-
-            </div>
-        <div class="col">{{ column2 }}</div>
-        <div class="col">{{ column3 }}</div>
+        </div>
+        <div class="col">
+            <ul>
+                <li class="cards" v-for="card in column2"><p>{{ card.title }}</p>
+                    <ul>
+                        <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
+                            <input @click="t.completed = true" 
+                            class="checkbox" type="checkbox"
+                            :disabled="t.completed">
+                            <p :class="{completed: t.completed}">{{t.title}}</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <ul>
+                <li class="cards" v-for="card in column3"><p>{{ card.title }}</p><p>{{ card.status }}</p><p>{{ card.date }}</p>
+                    <ul>
+                        <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
+                            <input @click="t.completed = true" 
+                            @click="newStatus"
+                            class="checkbox" type="checkbox"
+                            :disabled="t.completed">
+                            <p :class="{completed: t.completed}">{{t.title}}</p>
+                            
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
     </div>
 
@@ -50,7 +74,26 @@ Vue.component('cols', {
         })
     },
     methods: {
-
+        newStatus(id) {
+            let count = 0
+            let comtask = 0
+            for (let i; i < 5; i++) {
+                count += 1
+                if (this.column1.card.subtasks.completed === true) {
+                    comtask += 1
+                }
+            }
+            if (comtask/count*100 >= 50) {
+                this.column1.card.status = 1
+                this.column2.push(id)
+                this.column1.pop(id)
+            } else if (comtask/count*100 === 100) {
+                this.column1.card.status = 2
+                this.column3.push(id)
+                this.column2.pop(id)
+                this.column3.card.date = new Date()
+            }
+        }
     },
 
     computed: {
@@ -65,6 +108,18 @@ Vue.component('cols', {
             subtasks: {
                 type: Array,
                 required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: false
+            },
+            status: {
+                type: Number,
+                required: true
             }
         },
     },
@@ -123,7 +178,9 @@ Vue.component('newcard', {
                             {title: this.subtask2, completed: false},
                             {title: this.subtask3, completed: false},
                             {title: this.subtask4, completed: false},
-                            {title: this.subtask5, completed: false}]
+                            {title: this.subtask5, completed: false}],
+                date: null,
+                status: 0
             }
             eventBus.$emit('card-submitted', card)
             this.title = null
